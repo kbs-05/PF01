1ec3f9992f3498d2ca9cafd68694021ccf6b50e0
kbs-05 <155669340+kbs-05@users.noreply.github.com>
2025-09-01 17:46:26 +0200

    Update PaymentHistory.tsx

--- components/PaymentHistory.tsx
+++ components/PaymentHistory.tsx
@@ -13,7 +13,7 @@ type Remainder = {
 type Payment = {
   id: string;
   studentName: string;
-  receiptNumber: string;
+  studentMatricule: string;
   monthsPaid: string[]; // mois payés
   remainder?: Remainder; // reste éventuel
   amount: number; // montant payé
